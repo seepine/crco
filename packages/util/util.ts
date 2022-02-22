59334356b8fe587dbedf99c2aa640ae1aa80319d
seepine <seepine@outlook.com>
2022-02-22 14:02:40 +0800

    feat(util): add debounce

--- packages/util/util.ts
+++ packages/util/util.ts
@@ -1,3 +1,5 @@
+import { isBoolean, isNumber, isObject } from './is'
+
 export function deepClone(data: any): any {
   const type = Object.prototype.toString.call(data)
   let obj
@@ -26,4 +28,41 @@ export function deepClone(data: any): any {
     return data
   }
   return obj
+}
+
+interface DebounceOption {
+  wait?: number
+  leading?: boolean // 开始前调用,true
+  trailing?: boolean // 结束后调用,false
+}
+export function debounce(func: Function, option: DebounceOption | number) {
+  if (typeof func !== 'function') {
+    throw new TypeError('debounce need a function arguments')
+  }
+  let timeid: any = null
+  let wait = 500
+  let leading = true
+  let trailing = false
+
+  if (isNumber(option)) {
+    wait = option
+  } else if (isObject(option)) {
+    leading = isBoolean(option.leading) ? option.leading : leading
+    trailing = isBoolean(option.trailing) ? option.trailing : trailing
+  }
+  return () => {
+    if (timeid) {
+      clearTimeout(timeid)
+      return
+    }
+    if (leading) {
+      func()
+    }
+    timeid = setTimeout(() => {
+      if (trailing) {
+        func()
+      }
+      timeid = undefined
+    }, wait)
+  }
 }
