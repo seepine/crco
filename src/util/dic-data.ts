a41758606d5ffc1df942b6c5551a044e74cc0025
seepine <seepine@outlook.com>
2022-11-21 17:51:00 +0800

    feat(util): add initTreeDicData

--- src/util/dic-data.ts
+++ src/util/dic-data.ts
@@ -1,6 +1,6 @@
 import { filterProps } from './filter'
 import { isArray, isObject, isUndefined } from './is'
-import { deepClone } from './util'
+import { deepClone, runCallback } from './util'
 import { request } from './http'
 import { DicItem } from '../types/column'
 
@@ -43,4 +43,32 @@ export const initDicData = async (option: any): Promise<Array<DicItem>> => {
       data: item
     }
   })
+}
+
+export const initTreeDicData = async (option: any): Promise<Array<any>> => {
+  let dicData
+  try {
+    dicData = await runCallback(option.dicData, option)
+    // eslint-disable-next-line no-empty
+  } catch (e) {}
+  if (!isUndefined(dicData)) {
+    return dicData
+  }
+  if (!isUndefined(option.dicUrl)) {
+    try {
+      dicData = await request({
+        type: 'dic',
+        url: option.dicUrl,
+        params: option.params
+      })
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('init dicData error:', error)
+      return []
+    }
+  }
+  if (!isArray(dicData) || dicData.length <= 0) {
+    return []
+  }
+  return dicData
 }
