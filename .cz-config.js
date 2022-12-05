28a4a3332c8572b19a0a915ee8e1349e4edf9aa4
seepine <seepine@outlook.com>
2022-12-05 16:34:14 +0800

    chore(cz): fix cz template

--- .cz-config.js
+++ .cz-config.js
@@ -1,59 +1,44 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
-    { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     添加、修改测试用例' },
+    { value: 'fix', name: 'fix:       修复 bug' },
+    { value: 'feat', name: 'feat:      新增功能' },
+    { value: 'style', name: 'style:     代码格式（不影响功能，例如空格、分号等格式修正）' },
+    { value: 'docs', name: 'docs:      文档变更' },
+    { value: 'refactor', name: 'refactor:  代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'perf', name: 'perf:      性能优化' },
+    { value: 'test', name: 'test:      添加、修改测试用例' },
+    { value: 'chore', name: 'chore:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
+    { value: 'ci', name: 'ci:        修改 CI 配置、脚本' },
+    { value: 'deps', name: 'deps:      升级依赖' },
     {
-      value: 'build',
-      name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
-    },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert', name: 'revert:   回滚 commit' }
+      value: 'release',
+      name: 'release:   版本发布'
+    }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
-    ['components', '组件相关'],
-    ['hooks', 'hook 相关'],
-    ['utils', 'utils 相关'],
-    ['styles', '样式相关'],
-    ['deps', '项目依赖'],
-    ['auth', '对 auth 修改'],
-    ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-    ['custom', '以上都不是？我要自定义']
+    ['custom', '自定义范围，例如form、table'],
+    ['utils', 'utils 相关']
   ].map(([value, description]) => {
     return {
       value,
       name: `${value.padEnd(30)} (${description})`
     }
   }),
-
   // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
   // allowCustomScopes: true,
-
   // allowTicketNumber: false,
   // isTicketNumberRequired: false,
   // ticketNumberPrefix: 'TICKET-',
   // ticketNumberRegExp: '\\d{1,5}',
 
   // 针对每一个 type 去定义对应的 scopes，例如 fix
-  /*
+  /**/
   scopeOverrides: {
-    fix: [
-      { name: 'merge' },
-      { name: 'style' },
-      { name: 'e2eTest' },
-      { name: 'unitTest' }
-    ]
+    release: []
   },
-  */
 
   // 交互提示信息
   messages: {
@@ -68,11 +53,10 @@ module.exports = {
     confirmCommit: '确认提交？'
   },
 
-  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
-  allowBreakingChanges: ['feat', 'fix'],
-
+  // ['feat', 'fix']设置只有 type 选择了 feat 或 fix，才询问 breaking message
+  allowBreakingChanges: [],
   // 跳过要询问的步骤
-  // skipQuestions: ['body', 'footer'],
+  skipQuestions: ['body'],
 
   // subject 限制长度
   subjectLimit: 100,
