3d0ea80df7e14664a93173fb30e07f08d4530a50
seepine <seepine@outlook.com>
2022-12-09 17:27:58 +0800

    chore(commit): impr commit lint and husky

--- .cz-config.js
+++ .cz-config.js
@@ -1,26 +1,34 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'fix', name: 'fix:       修复 bug' },
-    { value: 'feat', name: 'feat:      新增功能' },
-    { value: 'style', name: 'style:     代码格式（不影响功能，例如空格、分号等格式修正）' },
-    { value: 'docs', name: 'docs:      文档变更' },
-    { value: 'refactor', name: 'refactor:  代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'perf', name: 'perf:      性能优化' },
-    { value: 'test', name: 'test:      添加、修改测试用例' },
-    { value: 'chore', name: 'chore:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'ci', name: 'ci:        修改 CI 配置、脚本' },
-    { value: 'deps', name: 'deps:      升级依赖' },
+    { value: 'improvement', name: 'improvement: 功能优化' },
+    { value: 'fix', name: 'fix:         修复 bug' },
+    { value: 'feat', name: 'feat:        新增功能' },
+    { value: 'style', name: 'style:       代码格式（不影响功能，例如空格、分号等格式修正）' },
+    { value: 'docs', name: 'docs:        文档变更' },
+    { value: 'refactor', name: 'refactor:    代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'perf', name: 'perf:        性能优化' },
+    { value: 'test', name: 'test:        添加、修改测试用例' },
     {
-      value: 'release',
-      name: 'release:   版本发布'
+      value: 'ci',
+      name: 'ci:          修改 CI 配置、脚本（如打包部署脚本、dockerfile等）'
+    },
+    { value: 'revert', name: 'revert:      回滚 commit' },
+    {
+      value: 'build',
+      name: 'build:       构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
+    },
+    {
+      value: 'chore',
+      name: 'chore:       版本发布或对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
     }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
-    ['custom', '自定义范围，例如form、table'],
-    ['utils', 'utils 相关']
+    ['custom', '自定义范围，例如sys/user'],
+    ['utils', 'utils 相关'],
+    ['router', 'router 相关']
   ].map(([value, description]) => {
     return {
       value,
@@ -35,16 +43,25 @@ module.exports = {
   // ticketNumberRegExp: '\\d{1,5}',
 
   // 针对每一个 type 去定义对应的 scopes，例如 fix
-  /**/
   scopeOverrides: {
-    release: []
+    chore: [
+      { value: 'release', name: 'release      版本发布' },
+      { value: 'custom', name: 'custom       自定义scope' }
+    ],
+    ci: [
+      { value: 'deploy', name: 'deploy       部署脚本' },
+      { value: 'docker', name: 'docker       docker相关' },
+      { value: 'custom', name: 'custom       自定义scope' }
+    ],
+    build: [
+      { value: 'deps', name: 'deps         依赖变更' },
+      { value: 'custom', name: 'custom       自定义scope' }
+    ]
   },
-
   // 交互提示信息
   messages: {
     type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
-    // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope：',
     subject: '填写简短精炼的变更描述：\n',
     body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
